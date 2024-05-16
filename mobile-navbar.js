73dc3e70b1ab2b85092c62b6756d3263790d0746
Raul Pereira <92689246+rauldpereira@users.noreply.github.com>
2024-05-16 17:10:46 -0300

    last commit
    aiai uiui vou apresentar

--- mobile-navbar.js
+++ mobile-navbar.js
@@ -1,44 +1,42 @@
 class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
+  constructor(mobileMenu, navList, navLinks) {
       this.mobileMenu = document.querySelector(mobileMenu);
       this.navList = document.querySelector(navList);
       this.navLinks = document.querySelectorAll(navLinks);
       this.activeClass = "active";
-  
+
       this.handleClick = this.handleClick.bind(this);
-    }
-  
-    animateLinks() {
+  }
+
+  animateLinks() {
       this.navLinks.forEach((link, index) => {
-        link.style.animation
-          ? (link.style.animation = "")
-          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
-              index / 7 + 0.3
-            }s`);
+          link.style.animation
+              ? (link.style.animation = "")
+              : (link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`);
       });
-    }
-  
-    handleClick() {
+  }
+
+  handleClick() {
       this.navList.classList.toggle(this.activeClass);
       this.mobileMenu.classList.toggle(this.activeClass);
       this.animateLinks();
-    }
-  
-    addClickEvent() {
+  }
+
+  addClickEvent() {
       this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
+  }
+
+  init() {
       if (this.mobileMenu) {
-        this.addClickEvent();
+          this.addClickEvent();
       }
       return this;
-    }
   }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-  );
-  mobileNavbar.init();
+}
+
+const mobileNavbar = new MobileNavbar(
+  ".mobile-menu",
+  ".nav-list",
+  ".nav-list li",
+);
+mobileNavbar.init();
